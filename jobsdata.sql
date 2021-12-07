5d8093280293f24b92898afbf3ba13ca4fb406b8
Pratik Kumar <pk4789218@gmail.com>
2021-12-07 12:06:03 +0530

    first commit

---
+++ jobsdata.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 07, 2021 at 07:32 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `internshala1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobsdata`
+--
+
+CREATE TABLE `jobsdata` (
+  `id` int(255) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `mobile` varchar(200) NOT NULL,
+  `jobs` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jobsdata`
+--
+
+INSERT INTO `jobsdata` (`id`, `username`, `email`, `mobile`, `jobs`) VALUES
+(1, 'pratik_kr_', 'pk4789218@gmail.com', '07004458803', '2'),
+(2, 'PRATIK4215', 'pk4789218@gmail.com', '07004458803', 'Web Developer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `jobsdata`
+--
+ALTER TABLE `jobsdata`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jobsdata`
+--
+ALTER TABLE `jobsdata`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
